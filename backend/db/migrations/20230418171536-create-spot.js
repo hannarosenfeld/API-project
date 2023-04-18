aec2c26b899a1af49267f4a93fe530535e2e1994
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2023-04-18 13:35:10 -0400

    Establish relationship to Users in Spots migration.

--- backend/db/migrations/20230418171536-create-spot.js
+++ backend/db/migrations/20230418171536-create-spot.js
@@ -10,7 +10,9 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       ownerId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'Users' }
       },
       address: {
         type: Sequelize.STRING
