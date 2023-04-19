6293c5567ff6378c0354a58b74b3e23ede56a410
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2023-04-19 11:41:18 -0400

    Create get route for all spots owned by current user.

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -5,11 +5,65 @@ const { handleValidationErrors } = require('../../utils/validation');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { Op } = require("sequelize");
 
-const { Spot } = require('../../db/models');
-
 const router = express.Router();
 
-const { SpotImage, Review } = require('../../db/models')
+const { User, Spot, SpotImage, Review } = require('../../db/models')
+
+
+// Get all Spots by owned by current User
+router.get('/current', async (req, res, next) => {
+    const { user } = req;
+    if (user) {
+      const safeUser = {
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        username: user.username,
+      };
+
+      const spots = await Spot.findAll({
+        where: {
+            ownerId: safeUser.id
+        }
+      })
+
+      const arr = []
+
+      for (let spot of spots) {
+         const previewImage = await spot.getSpotImages({
+          where: {
+              preview: true
+          }
+         });
+
+         // get average star rating...
+         const reviews = await Review.findAll()
+         const avgRating = await Review.count({
+              where: {
+                  spotId: {
+                      [Op.eq]: spot.id
+                  }
+              }
+         })
+         let counter = 0
+         for (let review of reviews) {
+          review = review.toJSON()
+          counter = counter + review.stars
+         }
+
+         spot = spot.toJSON()
+         spot.previewImage = previewImage[0].url
+         spot.avgRating = counter / avgRating
+
+         arr.push(spot)
+      }
+
+      return res.json({
+        spotsOwnedByCurrentUser: arr
+      });
+    } else return res.json({ error: "An authenticated user is required for a successful response. Please log in." });
+})
 
 // Get all Spots
 router.get('/', async (req, res, next) => {
