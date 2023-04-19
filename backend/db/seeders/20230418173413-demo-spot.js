5f866510f5493382c5f6fd95a5ecb0f81e5e92d6
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2023-04-19 11:46:21 -0400

    Add more seed data for spots.

--- backend/db/seeders/20230418173413-demo-spot.js
+++ backend/db/seeders/20230418173413-demo-spot.js
@@ -12,6 +12,18 @@ module.exports = {
     return queryInterface.bulkInsert(options, [
       {
         ownerId: 1,
+        address: "123 Disney Lane",
+        city: "San Francisco",
+        state: "California",
+        country: "United States of America",
+        lat: 37.7645358,
+        lng: -122.4730327,
+        name: "App Academy",
+        description: "Place where web developers are created",
+        price: 123,
+      },
+      {
+        ownerId: 1,
         address: "430 Belgrade St",
         city: "Philadelphia",
         state: "PA",
