8db0249205413944a2fa3be85b457ad686f6fc7e
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2023-04-19 17:10:05 -0400

    Fix post image route.

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -242,8 +242,9 @@ router.post('/:spotId/images', async (req, res, next) => {
     const { spotId } = req.params
     const spot = await Spot.findByPk(spotId)
     console.log(spot)
-    if (spot.ownerId !== user.id) res.json({ "message": "You must be the owner of the spot in order to upload a picture." })
+
     if (spot) {
+        if (spot.ownerId !== user.id) res.json({ "message": "You must be the owner of the spot in order to upload a picture." })
         if (url, preview) {
         const newImage = await SpotImage.create({
             spotId: spot.id,
