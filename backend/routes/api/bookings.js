5997f02759aa62455fcd61513b4adfc9f48eebea
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2023-04-21 12:49:02 -0400

    Edit a Booking.

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -22,7 +22,44 @@ router.get('/current', requireAuth, async (req, res, next) => {
     })
 
     res.json(bookings)
+})
 
+// Edit a Booking
+router.put('/:bookingId', requireAuth, async (req, res, next) => {
+    const { bookingId } = req.params
+    const { user } = req
+    let { startDate, endDate } = req.body
+
+    const booking = await Booking.findByPk(bookingId)
+    const sessionUser = await User.findByPk(user.id)
+
+    startDate = new Date(startDate)
+    endDate = new Date(endDate)
+
+    if (bookingId) {
+        if (sessionUser.id === booking.userId) {
+            if (startDate >= endDate) {
+                res.statusCode = 400
+                return res.json({ message: "endDate cannot be on or before startDate"})
+            }
+            if (endDate <= new Date()) {
+                res.statusCode = 400
+                return res.json({ message: "Past bookings can't be modified"})
+            }
+
+            if (startDate) booking.startDate = startDate
+            if (endDate) booking.endDate = endDate
+
+            res.json(booking)
+
+        } else {
+            res.statusCode = 403
+            return res.json({message: "You must be the owner of this spot in order to edit it."})
+        }
+    } else {
+        result.status = 404
+        res.json({ message: "Booking does not exist"})
+    }
 })
 
 
