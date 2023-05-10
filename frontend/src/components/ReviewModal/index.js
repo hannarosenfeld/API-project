a55c8f7baffbd31d688ea32f2588816d01364d2b
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2023-05-10 18:30:24 -0400

    Star rating.

--- frontend/src/components/ReviewModal/index.js
+++ frontend/src/components/ReviewModal/index.js
@@ -1,8 +1,10 @@
+import "./ReviewModal.css"
+import { useEffect, useState } from 'react';
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
 
-import "./ReviewModal.css"
-import { useEffect, useState } from 'react';
+
+
 
 
 export default function ReviewModal() {
@@ -24,7 +26,48 @@ export default function ReviewModal() {
                     style={{height: "17em", background: "aliceblue", marginBottom: "2em"}}
                 />
             <div className="stars" style={{margin: "0 auto"}}>
-            {/* <Stars/> */}
+                <div className="rating-input" style={{display: "flex"}}>
+                    <div
+                        className={activeRating >= 1 ? "filled" : "empty"}
+                        onMouseEnter={() => { setActiveRating(1)} }
+                        onMouseLeave={() => { setActiveRating(rating)} }
+                        onClick={() => { setRating(1)} }
+                    >
+                        <i className="fa fa-star"></i>
+                    </div>
+                    <div
+                        className={activeRating >= 2 ? "filled" : "empty"}
+                        onMouseEnter={() => { setActiveRating(2)} }
+                        onMouseLeave={() => { setActiveRating(rating)} }
+                        onClick={() => { setRating(2)} }
+                    >
+                        <i className="fa fa-star"></i>
+                    </div>
+                    <div
+                        className={activeRating >= 3 ? "filled" : "empty"}
+                        onMouseEnter={() => { setActiveRating(3)} }
+                        onMouseLeave={() => { setActiveRating(rating)} }
+                        onClick={() => { setRating(3)} }
+                    >
+                        <i className="fa fa-star"></i>
+                    </div>
+                    <div
+                        className={activeRating >= 4 ? "filled" : "empty"}
+                        onMouseEnter={() => { setActiveRating(4)} }
+                        onMouseLeave={() => { setActiveRating(rating)} }
+                        onClick={() => { setRating(4)} }
+                    >
+                        <i className="fa fa-star"></i>
+                    </div>
+                    <div
+                        className={activeRating >= 5 ? "filled" : "empty"}
+                        onMouseEnter={() => { setActiveRating(5) }}
+                        onMouseLeave={() => { setActiveRating(rating)} }
+                        onClick={() => { setRating(5)} }
+                    >
+                        <i className="fa fa-star"></i>
+                    </div>
+                    </div>
             </div>
             <button style={{padding: "0.5em", height: "2em", margin: "1em"}}>Submit your Review</button>
             </form>
