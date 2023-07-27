17154b7b6cfbf993263071aeebe6b73be091c2f1
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2023-07-27 09:46:04 -0400

    Update review.

--- frontend/src/store/reviews.js
+++ frontend/src/store/reviews.js
@@ -3,7 +3,7 @@ import { getOneSpot } from "./spots";
 
 export const LOAD_REVIEWS = 'reviews/LOAD_REVIEWS'
 export const DELETE_REVIEW = 'reviews/DELETE_REVIEW'
-
+export const EDIT_REVIEW = 'reviews/EDIT_REVIEW'
 
 const removeReview = (reviewId) => ({
   type: DELETE_REVIEW,
@@ -13,8 +13,42 @@ const removeReview = (reviewId) => ({
 const loadReviews = (reviews) => ({
   type: LOAD_REVIEWS,
   reviews,
-
 })
+
+const editReviewAction = (review) => ({
+  type: EDIT_REVIEW,
+  review
+})
+
+export const editReviewThunk = (reviewData, reviewId, spotId) => async (dispatch) => {
+  try {
+    const res = await csrfFetch(`/api/reviews/${reviewId}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(reviewData),
+    });
+
+    if (!res.ok) {
+      const err = await res.json();
+      throw err;
+    }
+
+    const editedReview = await res.json();
+
+    // Dispatch the action with the edited review object
+    dispatch(editReviewAction(editedReview));
+
+    // Fetch updated reviews and spot information after the edit
+    await dispatch(getReviews(spotId));
+    await dispatch(getOneSpot(spotId));
+
+    return editedReview;
+  } catch (err) {
+    // Handle errors here or rethrow them to be handled in the component
+    throw err;
+  }
+};
+
 
 
 export const getReviews = (spotId) => async (dispatch) => {
@@ -61,7 +95,7 @@ const initialState = {};
 const reviewsReducer = (state = initialState, action) => {
   switch (action.type) {
     case LOAD_REVIEWS:
-      const reviewsState= {};
+      const reviewsState = {};
       action.reviews.Reviews.forEach(review => {
         reviewsState[review.id] = review;
       });
@@ -70,6 +104,12 @@ const reviewsReducer = (state = initialState, action) => {
         const newState = { ...state };
         delete newState[action.reviewId];
         return newState;
+      case EDIT_REVIEW:
+        console.log("action",action)
+        return {
+          ...state,
+          [action.review.id]: action.review
+        };
       default:
         return state;
     }
